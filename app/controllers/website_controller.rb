141eb45a1cfce9f2721362da62e1e2ed4c1c68d9
Mo Bitar <me@bitar.io>
2018-09-27 14:03:35 -0500

    Working commit

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -1,32 +1,65 @@
 class WebsiteController < ApplicationController
 
+  before_action {
+    build_menu
+  }
+
   def index
 
-    @structure = {
+  end
 
-    }
 
+  def template
+    path = params[:path]
+    root_path = "website/templates"
+
+    @menu_structure.each do |section|
+      section[:pages].each do |page|
+        if page[:path] == path
+          @template_path = "#{root_path}/#{page[:template]}"
+          @title = page[:title]
+        end
+      end
+    end
+
+  end
+
+  private
+
+  def build_menu
     @menu_structure = [
       {
         title: "Standard File",
         pages: [
           {
-            title: "Self hosting Standard File",
-            template: "standard-file/_self-hosting-standard-file"
-          }
+            title: "Self Hosting with EC2 and Nginx",
+            template: "standard-file/ec2-nginx.html"
+          },
+          {
+            title: "Self Hosting with Docker",
+            template: "standard-file/docker.html"
+          },
         ]
       },
       {
         title: "Extensions",
         pages: [
-          {
-            title: "Self hosting Standard File",
-            template: "standard-file/_self-hosting-standard-file"
-          }
+
         ]
       }
     ]
 
+    compute_paths_for_structure(@menu_structure)
   end
+
+  def compute_paths_for_structure(structure)
+    structure.each do |section|
+      section[:pages].each do |page|
+        path = "#{section[:title].parameterize}/#{page[:title].parameterize}"
+        page[:path] = path
+      end
+    end
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
   root "website#index"
 
+  match '*path' => 'website#template', :via => "get"
+
 end
