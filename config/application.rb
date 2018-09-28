432fbb94f141575046a35fc8abd5ebcada03ab65
Mo Bitar <me@bitar.io>
2018-09-28 13:32:41 -0500

    Updates

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
 module Docs
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
